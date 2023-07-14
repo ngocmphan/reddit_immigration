cd8b905b8663ce71e78ef0569e930cec82e187a9
ngocmphan <ngocm.phan@gmail.com>
2023-07-14 16:49:07 -0400

    Added contact info

---
+++ pages/contact_info.py
@@ -0,0 +1,15 @@
+# Import libraries
+import streamlit as st
+
+
+# Contact information
+st.markdown("# Contact Information")
+st.sidebar.markdown("# Contact Info")
+
+st.markdown("## Project Page:")
+st.markdown("[Github Project: Reddit Immigration](https://github.com/ngocmphan/reddit_immigration)")
+
+st.markdown("## Connect with me - Linkedin")
+st.markdown("[Linkedin - Alicia .P](https://www.linkedin.com/in/ngocmphan/)")
+
+st.markdown("Please don't hesitate to contact me if you want to talk about the project, collaborate or discuss on different ideas. Thank you for reading.")
