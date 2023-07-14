ba5b9f26ffa252fd835f200f1e203333d60f5cd2
ngocmphan <ngocm.phan@gmail.com>
2023-07-14 15:27:17 -0400

    Added page 3

---
+++ pages/page_3.py
@@ -0,0 +1,23 @@
+# Import libraries
+import streamlit as st
+
+
+# Page 3: Data sources, references and data limitation
+st.markdown("# References & Others")
+st.sidebar.markdown("# Page 3")
+
+st.markdown("## References")
+
+
+
+st.markdown("## Tools")
+st.markdown("- Pandas")
+st.markdown("- Plotly")
+st.markdown("- Streamlit")
+st.markdown("- WordCloud")
+st.markdown("- PMAW - Pushift Multithread API Wrapper")
+st.markdown("- NLTK Vader")
+
+st.markdown("## Data Sources & Data Limitation")
+
+
