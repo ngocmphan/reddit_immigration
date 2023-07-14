100bc7447a74e5f33f53567e70f43f428eaaf24d
ngocmphan <ngocm.phan@gmail.com>
2023-07-14 15:34:25 -0400

    Added page 3

--- pages/page_3.py
+++ pages/page_3.py
@@ -7,7 +7,9 @@ st.markdown("# References & Others")
 st.sidebar.markdown("# Page 3")
 
 st.markdown("## References")
+st.markdown("Immigration, Refugees and Citizenship Canada. Operational Processing - Monthly IRCC Updates. Extracted on July 2023 via the following link: https://open.canada.ca/data/en/dataset/9b34e712-513f-44e9-babf-9df4f7256550")
 
+st.markdown("Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text. Eighth International Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.")
 
 
 st.markdown("## Tools")
